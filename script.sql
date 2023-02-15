da4fc696a141a1369ccb0f0dad798d91b8e01045
Mulori <murilogarcia.hr@gmail.com>
2023-02-14 22:18:16 -0300

    update

--- script.sql
+++ script.sql
@@ -98,22 +98,6 @@ create table usuario_escola(
 	constraint fk_codigo_escolaa foreign key (codigo_escola) references escola (codigo)	
 );
 
-create table usuario_periodo(
-	codigo int AUTO_INCREMENT primary key,
-	codigo_periodo int not null,
-	codigo_usuario int not null,
-	constraint fk_codigo_periodo foreign key (codigo_periodo) references periodo (codigo),
-	constraint fk_codigo_usuarioo foreign key (codigo_usuario) references usuario (codigo)
-);
-
-create table usuario_tipo_transporte(
-	codigo int AUTO_INCREMENT primary key,
-	codigo_tipo_transporte int not null,
-	codigo_usuario int not null,
-	constraint fk_codigo_tipo_transporte foreign key (codigo_tipo_transporte) references tipo_transporte (codigo),
-	constraint fk_codigo_usuariooo_tipo_transporte foreign key (codigo_usuario) references usuario (codigo)
-);
-
 create table rota_usuario(
 	codigo int AUTO_INCREMENT primary key,
 	nome_rota varchar(100) not null,
@@ -162,17 +146,15 @@ create table veiculo_motorista_imagem(
 	constraint fk_imagem_veiculo_motorista foreign key (codigo_veiculo_motorista) references veiculo_motorista (codigo)
 );
 
-create table responsavel_escolar (
+create table responsavel_aluno (
 	codigo int AUTO_INCREMENT primary key,
 	codigo_motorista int not null,
-	codigo_periodo int not null,
 	nome_completo varchar(255) not null,
 	cpf_cnpj varchar(14) not null,
 	data_nascimento date,
 	email varchar(255) not null,
 	telefone varchar(30),
 	celular varchar(30) not null,
-	valor_mensalidade decimal(14,2) not null,
 	cadastrado datetime not null,
 	alterado datetime not null,
 	ativo int not null,
@@ -181,53 +163,70 @@ create table responsavel_escolar (
 );
 
 
-create table aluno_responsavel_escolar(
+create table aluno(
 	codigo int AUTO_INCREMENT primary key,
+	codigo_motorista int not null,
 	codigo_escola int not null,
 	codigo_periodo int not null,
+	codigo_tipo_transporte int not null,
 	codigo_responsavel int not null,
 	codigo_veiculo int not null,
-	codigo_tipo_transporte int not null,
 	nome_completo varchar(255) not null,
 	documento varchar(50),
-	data_nascimento date,
+	data_nascimento date not null,
 	valor_mensalidade decimal(14,2) not null,
+	vencimento_mensalidade datetime not null,
 	observacao varchar(5000),
-	constraint fk_codigo_responsavel_escolar foreign key (codigo_responsavel) references responsavel_escolar (codigo),
 	constraint fk_codigo_escola_aluno foreign key (codigo_escola) references escola (codigo),
 	constraint fk_codigo_periodo_aluno foreign key (codigo_periodo) references periodo (codigo),
-	constraint fk_codigo_veiculo_aluno foreign key (codigo_veiculo) references veiculo_motorista (codigo)
+	constraint fk_codigo_veiculo_aluno foreign key (codigo_veiculo) references veiculo_motorista (codigo),
+	constraint fk_codigo_tipo_transporte_aluno foreign key (codigo_tipo_transporte) references tipo_transporte (codigo),
+	constraint fk_codigo_motorista_aluno foreign key (codigo_motorista) references usuario (codigo)
 );
 
 create table mensalidade(
 	codigo int AUTO_INCREMENT primary key,
 	codigo_motorista int not null,
-	codigo_responsavel int not null,
+	codigo_aluno int not null,
 	ano int not null,
 	janeiro_valor decimal(14,2) not null,
 	janeiro_recebido decimal(14,2) not null,
+	janeiro_pagamento datetime,
 	fevereiro_valor decimal(14,2) not null,
 	fevereiro_recebido decimal(14,2) not null,
+	fevereiro_pagamento datetime,
 	marco_valor decimal(14,2) not null,
 	marco_recebido decimal(14,2) not null,
+	marco_pagamento datetime,
 	abril_valor decimal(14,2) not null,
 	abril_recebido decimal(14,2) not null,
+	abril_pagamento datetime,
 	maio_valor decimal(14,2) not null,
 	maio_recebido decimal(14,2) not null,
+	maio_pagamento datetime,
 	junho_valor decimal(14,2) not null,
 	junho_recebido decimal(14,2) not null,
+	junho_pagamento datetime,
 	julho_valor decimal(14,2) not null,
 	julho_recebido decimal(14,2) not null,
+	julho_pagamento datetime,
 	agosto_valor decimal(14,2) not null,
 	agosto_recebido decimal(14,2) not null,
+	agosto_pagamento datetime,
 	setembro_valor decimal(14,2) not null,
 	setembro_recebido decimal(14,2) not null,
+	setembro_pagamento datetime,
 	outubro_valor decimal(14,2) not null,
 	outubro_recebido decimal(14,2) not null,
+	outubro_pagamento datetime,
 	novembro_valor decimal(14,2) not null,
 	novembro_recebido decimal(14,2) not null,
+	novembro_pagamento datetime,
 	dezembro_valor decimal(14,2) not null,
-	dezembro_recebido decimal(14,2) not null
+	dezembro_recebido decimal(14,2) not null,
+	dezembro_pagamento datetime,
+	constraint fk_codigo_motorista_mensalidade foreign key (codigo_motorista) references usuario (codigo),
+	constraint fk_codigo_aluno_mensalidade foreign key (codigo_aluno) references aluino (codigo)
 );
 
 create table usuario_financeiro (
---
+++ src/rotas/escola/index.js
@@ -0,0 +1,34 @@
+const rota = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const md5 = require("md5");
+const middleware = require("../../middlewares/autenticacao");
+
+rota.use(middleware);
+
+rota.get("/api/v1/escola/cidade/:nome", async (req, res) => {
+  const nome = req.params.nome;
+
+  if (!nome) {
+    return res.status(400).send("Solicitação incorreta.");
+  }
+
+  await prisma.escola
+    .findMany({
+      where: {
+        cidade_escola: nome.trim(),
+      },
+      orderBy: {
+        nome_escola: "asc",
+      },
+    })
+    .then((e) => {
+      return res.status(200).json(e);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json("Ocorreu um erro do servidor");
+    });
+});
+
+module.exports = rota;
